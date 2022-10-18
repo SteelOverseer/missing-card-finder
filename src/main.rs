51a779a1ad69fea0fca539cad2c2ffb8e1155142
Doug Schultz <douglasschultz1@gmail.com>
2022-10-17 22:29:02 -0400

    Read collection csv

--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,9 @@
-use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap};
+use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap, error::Error, process};
 use regex::Regex;
 use substring::Substring;
 
 fn main() {
-    // const COLLECTION_PATH: &str = "C:\\Users\\Doug\\Documents\\Magic\\Collection.coll2";
+    const COLLECTION_PATH: &str = "C:\\Users\\Doug\\Documents\\Magic\\collection.csv";
     const DECK_PATHS: [&str; 8] = [
         "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Arcades.dec",
         "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Dragons.dec",
@@ -15,20 +15,20 @@ fn main() {
         "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Tasha.dec",
     ];
     let mut deck_contents:HashMap<String, u32> = HashMap::new();
-    // let collection_contents:HashMap<String, u32> = HashMap::new();
+    let mut collection_contents:HashMap<String, u32> = HashMap::new();
 
     for path in DECK_PATHS {
-        load_from_file(path, &mut deck_contents);
+        load_deck_file(path, &mut deck_contents);
     }
     
-    // load_from_file(COLLECTION_PATH, collection_contents);
-
-    for (key, value) in deck_contents {
-        println!("{}: {}", key, value);
+    if let Err(err) = load_collection_file(COLLECTION_PATH, &mut collection_contents) {
+        println!("{}", err);
+        process::exit(1);
     }
+
 }
 
-fn load_from_file(file_path: &str, contents: &mut HashMap<String, u32>) {
+fn load_deck_file(file_path: &str, contents: &mut HashMap<String, u32>) {
     let file = File::open(file_path).expect("Could not read file {file_path}");
     let reader = BufReader::new(file);
     let line_reg = Regex::new(r"/").unwrap();
@@ -40,11 +40,29 @@ fn load_from_file(file_path: &str, contents: &mut HashMap<String, u32>) {
             let quantity = line.substring(quantity_match.start(), quantity_match.end()).parse::<u32>().unwrap();
             let card_name = line.substring(quantity_match.end() + 1, line.len()).to_string();
 
-            if contents.contains_key(&card_name) {
-                *contents.get_mut(&card_name).unwrap() += quantity;
-            } else {
-                contents.insert(card_name, quantity);
-            }
+            set_hash(card_name, quantity, contents);
         }
+    }
+}
+
+fn load_collection_file(file_path: &str, contents: &mut HashMap<String, u32>) -> Result<(), Box<dyn Error>> {
+    let file = File::open(file_path)?;
+    let mut rdr = csv::Reader::from_reader(file);
+    
+    for result in rdr.records() {
+        let record = result?;
+        let quantity = record[0].parse::<u32>().unwrap();
+        let card_name = record[3].to_string();
+        
+        set_hash(card_name, quantity, contents);
+    }
+    Ok(())
+}
+
+fn set_hash(card_name: String, quantity: u32, contents: &mut HashMap<String, u32> ) {
+    if contents.contains_key(&card_name) {
+        *contents.get_mut(&card_name).unwrap() += quantity;
+    } else {
+        contents.insert(card_name, quantity);
     }
 }
