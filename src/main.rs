a42f15d23e29cf295a0054b41ac6ab5a1fa14c9e
Doug Schultz <douglasschultz1@gmail.com>
2022-10-19 00:13:50 -0400

    Traverse directory to find decks

--- src/main.rs
+++ src/main.rs
@@ -1,24 +1,33 @@
 use std::{fs::File, io::{BufReader, BufRead}, collections::HashMap, error::Error, process};
 use regex::Regex;
 use substring::Substring;
+use walkdir::{DirEntry, WalkDir};
 
-fn main() {
+static FORMATS: [&str; 3] = ["Standard", "Legacy", "Frontier"];
+static TRACKED_MODERN_DECKS: [&str; 5] = ["Affinity.dec", "DeathsShadow-Grixis.dec", "GiftsStorm.dec", "Spirits-UW.dec", "Tron-G.dec"];
+
+fn main() -> Result<(), Box<dyn Error>> {
     const COLLECTION_PATH: &str = "C:\\Users\\Doug\\Documents\\Magic\\collection.csv";
-    const DECK_PATHS: [&str; 8] = [
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Arcades.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Dragons.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\GroupHug.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Narset.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Neheb.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Rebels.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Stax.dec",
-        "C:\\Users\\Doug\\Documents\\Magic\\Decks\\Commander\\Tasha.dec",
-    ];
+   
     let mut deck_contents:HashMap<String, u32> = HashMap::new();
     let mut collection_contents:HashMap<String, u32> = HashMap::new();
+    let mut format = String::from("");
 
-    for path in DECK_PATHS {
-        load_deck_file(path, &mut deck_contents);
+    let walker = WalkDir::new("C:\\Users\\Doug\\Documents\\Magic\\Decks").into_iter();
+    for entry in walker.filter_entry(|e| !is_untracked_format(e)) {
+        let file_name = String::from(entry?.file_name().to_string_lossy());
+
+        if file_name.contains(".dec") {
+            if format == "Modern" && TRACKED_MODERN_DECKS.contains(&&file_name[..]) {
+                let file_path = format!("C:\\Users\\Doug\\Documents\\Magic\\Decks\\{format}\\{file_name}");
+                load_deck_file(file_path, &mut deck_contents);
+            } else if format != "Modern" {
+                let file_path = format!("C:\\Users\\Doug\\Documents\\Magic\\Decks\\{format}\\{file_name}");
+                load_deck_file(file_path, &mut deck_contents);
+            }
+        } else if file_name != "Decks" {
+            format = file_name.to_owned();
+        }
     }
     
     if let Err(err) = load_collection_file(COLLECTION_PATH, &mut collection_contents) {
@@ -27,14 +36,14 @@ fn main() {
     }
 
     for (card_name, quantity) in deck_contents {
-        let mut needed_quantity = 0;
+        let needed_quantity;
 
         if collection_contents.contains_key(&card_name) {
             let owned_quantity = *collection_contents.get_mut(&card_name).unwrap();
             needed_quantity = owned_quantity as i32 - quantity as i32;
         } else {
             needed_quantity = quantity as i32;
-            println!("Missing {needed_quantity} {card_name}")
+           println!("Missing {needed_quantity} {card_name}")
         }
 
         if needed_quantity < 0 {
@@ -42,9 +51,10 @@ fn main() {
         }
     }
 
+    Ok(())
 }
 
-fn load_deck_file(file_path: &str, contents: &mut HashMap<String, u32>) {
+fn load_deck_file(file_path: String, contents: &mut HashMap<String, u32>) {
     let file = File::open(file_path).expect("Could not read file {file_path}");
     let reader = BufReader::new(file);
     let line_reg = Regex::new(r"/").unwrap();
@@ -81,4 +91,11 @@ fn set_hash(card_name: String, quantity: u32, contents: &mut HashMap<String, u32
     } else {
         contents.insert(card_name, quantity);
     }
+}
+
+fn is_untracked_format(entry: &DirEntry) -> bool {
+    entry.file_name()
+         .to_str()
+         .map(|s| FORMATS.contains(&s) || s.starts_with("."))
+         .unwrap_or(false)
 }
